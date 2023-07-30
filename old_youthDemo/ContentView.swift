bacd34edd338b91da77948f1ce7135c29cf59440
Hanilbert <hantianang@gmail.com>
2023-07-30 22:27:57 +0800

    Initial Commit

---
+++ old_youthDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  old_youthDemo
+//
+//  Created by 韩天昂 on 30/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ old_youthDemo/old_youthDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  old_youthDemoApp.swift
+//  old_youthDemo
+//
+//  Created by 韩天昂 on 30/7/23.
+//
+
+import SwiftUI
+
+@main
+struct old_youthDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
